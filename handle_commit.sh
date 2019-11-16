a5cbb26ef0ee803a6841697dd27d34a78e30e5fe
alexandermeyer <alexm@mighty.com>
2019-11-16 14:15:11 -0500

    Fix formatting in handle commit script

--- handle_commit.sh
+++ handle_commit.sh
@@ -4,21 +4,21 @@
 if [ ! -d ".git" ]; then
   git init
   if (( $? )); then
-      echo "Unable to initialize your directory"
-      exit 1
+    echo "Unable to initialize your directory"
+    exit 1
   fi
 fi
 
 git add .
 if (( $? )); then
-echo "Unable to stage files"
-exit 1
+  echo "Unable to stage files"
+  exit 1
 fi
 
 git commit -m "$1"
 if (( $? )); then
-echo "Unable to commit"
-exit 1
+  echo "Unable to commit"
+  exit 1
 fi
 
 exit 0
